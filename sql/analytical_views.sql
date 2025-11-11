505d485086e90f6c41b5cd2724e157c9247fb83b
martinkivisikk <martin.kivisikk@gmail.com>
2025-11-11 18:22:27 +0200

    Add roles and views

---
+++ sql/analytical_views.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE VIEW dataeng.vw_trip_summary_full AS
+SELECT
+    f.trip_id,
+    f.start_date_key,
+    f.end_date_key,
+    s.station_name AS start_station_name,
+    e.station_name AS end_station_name,
+    f.rider_type,
+    f.bike_type,
+    f.trip_duration,
+    w.apparent_temp,
+    w.humidity_category,
+    w.wind_category
+FROM dataeng.Fact_Bike_Trip f
+LEFT JOIN dataeng.Dim_Station s ON f.start_station_key = s.station_key
+LEFT JOIN dataeng.Dim_Station e ON f.end_station_key = e.station_key
+LEFT JOIN dataeng.Dim_Weather w ON f.weather_key = w.weather_key;
+
+CREATE OR REPLACE VIEW dataeng.vw_trip_summary_masked AS
+SELECT
+    f.trip_id,
+    f.start_date_key,
+    f.end_date_key,
+    MD5(s.station_name) AS start_station_name_masked,   
+    MD5(e.station_name) AS end_station_name_masked,
+    'rider' AS rider_type_masked,               
+    concat('*_', right(f.bike_type, 4)) as bike_type_masked,
+    f.trip_duration,
+    w.apparent_temp,
+    w.humidity_category,
+    w.wind_category
+FROM dataeng.Fact_Bike_Trip f
+LEFT JOIN dataeng.Dim_Station s ON f.start_station_key = s.station_key
+LEFT JOIN dataeng.Dim_Station e ON f.end_station_key = e.station_key
+LEFT JOIN dataeng.Dim_Weather w ON f.weather_key = w.weather_key;
