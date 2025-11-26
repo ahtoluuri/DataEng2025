db4e3dd8fe52223455aca4910bc6b2ebceed3d77
martinkivisikk <martin.kivisikk@gmail.com>
2025-11-26 10:10:07 +0200

    Made roles sql script rerunnable

--- sql/task_3_4_roles.sql
+++ sql/task_3_4_roles.sql
@@ -1,19 +1,19 @@
-CREATE ROLE role_openmetadata;
+-- ROLES
+CREATE ROLE IF NOT EXISTS role_openmetadata;
+CREATE ROLE IF NOT EXISTS role_superset_full;
 
-CREATE USER service_openmetadata IDENTIFIED WITH sha256_password BY 'omd_very_secret_password';
+-- USERS
+CREATE USER IF NOT EXISTS service_openmetadata
+    IDENTIFIED WITH sha256_password BY 'omd_very_secret_password';
+CREATE USER IF NOT EXISTS service_superset_full
+    IDENTIFIED WITH sha256_password BY 'superset_very_secret_password';
 
+-- GRANTS
 GRANT role_openmetadata TO service_openmetadata;
-
 GRANT SELECT, SHOW ON system.* to role_openmetadata;
-
 GRANT SELECT ON dataeng.* TO role_openmetadata;
 GRANT SELECT ON citibike.* TO role_openmetadata;
 
-CREATE ROLE role_superset_full;
-
-CREATE USER service_superset_full IDENTIFIED WITH sha256_password BY 'superset_very_secret_password';
-
 GRANT role_superset_full TO service_superset_full;
-
 GRANT SELECT ON dataeng.* TO role_superset_full;
 GRANT SELECT ON citibike.* TO role_superset_full;
